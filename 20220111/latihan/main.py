edd005ba360211303bdc5a352bce96b9cf4cb63c
learning-enisda <learningenisda@gmail.com>
2022-01-13 07:54:37 +0800

    init

--- 20220111/latihan/main.py
+++ 20220111/latihan/main.py
@@ -1,16 +1,17 @@
 # use for loop to iterate from 0 to 100 an print even numbers then print only odd number
+# Enisda Libra Kelana (11203462010011)
 
-
-# Python program to print Even Numbers in given range
+# Print Even Numbers
 start, end = 1, 100
 # iterating each number in list
-for num in range(start, end):
+for num in range(start, end + 1):
+	
 	# checking condition
 	if num % 2 == 0:
-		print(num, end=" ")
+		print(num, end = " ")
 
 
-# Python program to print odd Numbers in given range
+# Print odd Numbers
 start, end = 1, 100
 # iterating each number
 for num in range(start, end + 1):
